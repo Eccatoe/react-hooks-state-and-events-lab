99dff038fa98a3de26a9fcd82ff9ed6f67b7ceec
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-02 17:44:21 -0500

    completed assignment-fully functional

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+  function handleItemClick(){
+    setInCart(!inCart)
+  }
+
+  const liClassName= inCart? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleItemClick} className="add">{inCart? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+//when button is clicked, change li className to in-cart vs ""
+//if not in cart, button text is "add to cart"
+//if in cart, button text is "remove from cart"
